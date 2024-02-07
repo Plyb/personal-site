4cd05413eeb65e667fe02087b29f74d6f89ccca0
Koby Lewis <kobyjlewis@gmail.com>
2024-02-06 18:13:54 -0700

    consistent oxford comma usage and fix misspelling

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,8 @@ function App() {
           </div>
           <div className='panel'>
             <h2>Koby Lewis</h2>
-            <p>I'm a software engineer persuing a PhD in programming languages and tools. I graduated with my Bachelor's in <a href='https://cs.byu.edu/'>Computer Science</a> from <a href='https://www.byu.edu/'>Brigham Young University</a> in 2023 and I have been working as a backend software engineer for <a href='https://www.award.co/'>Awardco</a> since then.</p>
-            <p>I'm interested in ways to make programmers' jobs easier, less error prone and more productive, mainly through the design of new languages, tools, and libraries. I also am interested in systems with emergent behavior and simulations. Outside of technical work, I enjoy hiking, tabletop games, reading, and studying linguistics.</p>
+            <p>I'm a software engineer pursuing a PhD in programming languages and tools. I graduated with my Bachelor's in <a href='https://cs.byu.edu/'>Computer Science</a> from <a href='https://www.byu.edu/'>Brigham Young University</a> in 2023 and I have been working as a backend software engineer for <a href='https://www.award.co/'>Awardco</a> since then.</p>
+            <p>I'm interested in ways to make programmers' jobs easier, less error prone, and more productive, mainly through the design of new languages, tools, and libraries. I also am interested in systems with emergent behavior and simulations. Outside of technical work, I enjoy hiking, tabletop games, reading, and studying linguistics.</p>
             <p>You can find my CV and ways to contact me in the header of this page!</p>
           </div>
         </div>
